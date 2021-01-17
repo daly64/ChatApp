afba029c4cc6e7eef470a878a97b2986c87f6c17
Daly <41022166+daly64@users.noreply.github.com>
2021-01-17 12:46:41 +0100

    add items to listview with send button

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.chatapp;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -14,7 +13,6 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +20,24 @@ public class MainActivity extends AppCompatActivity {
 
         EditText editText = findViewById(R.id.editText);
         ListView lv = findViewById(R.id.list_item);
+        Button send = findViewById(R.id.sendBtn);
+
         List<String> your_array_list = new ArrayList<String>();
 
+        send.setOnClickListener(v -> {
 
-        your_array_list.add("foo");
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<>(
-                        this,
-                        android.R.layout.simple_list_item_1,
-                        your_array_list);
+            String message = String.valueOf(editText.getText());
+            your_array_list.add(message);
 
-        lv.setAdapter(arrayAdapter);
+            ArrayAdapter<String> arrayAdapter =
+                    new ArrayAdapter<>(
+                            this,
+                            android.R.layout.simple_list_item_1,
+                            your_array_list);
+
+            lv.setAdapter(arrayAdapter);
+        });
+
 
     }
 
