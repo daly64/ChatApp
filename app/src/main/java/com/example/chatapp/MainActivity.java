400a8f21546205b4e5dc4c6afe01763f24256ee9
Daly <41022166+daly64@users.noreply.github.com>
2021-01-17 01:09:00 +0100

    primary layout

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.chatapp;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editText);
+        ListView lv = findViewById(R.id.list_item);
+        List<String> your_array_list = new ArrayList<String>();
+
+
+        your_array_list.add("foo");
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        your_array_list);
+
+        lv.setAdapter(arrayAdapter);
+
     }
+
+
 }
