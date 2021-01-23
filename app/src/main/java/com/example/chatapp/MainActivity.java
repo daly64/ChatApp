8004612f732abd28a1296da2e8c81d6ff1be2abe
Daly <41022166+daly64@users.noreply.github.com>
2021-01-23 14:44:34 +0100

    setting up auto login 1/2

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,64 +1,33 @@
 package com.example.chatapp;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.chatapp.models.Message;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    RecyclerView.Adapter adapter;
-    RecyclerView.LayoutManager layoutManager;
+    CircleImageView profile_image;
+    TextView username;
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText = findViewById(R.id.editText);
-//        Button send = findViewById(R.id.sendBtn);
-
-
-        recyclerView = findViewById(R.id.list_item);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        ArrayList<Message> messagesList = new ArrayList<>();
-
-        Context context = this;
-        editText.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == EditorInfo.IME_ACTION_SEND) {
-                // Send message
-//                ToolBox.sendMessage(editText,messagesList,context,recyclerView);
-
-
-                return false;
-            }
-
-            return false;
-        });
-
-       /* send.setOnClickListener(v -> {
-
-            Message message = new Message(editText.getText().toString());
-            messagesList.add(message);
-            adapter = new MessageRecycler(this, messagesList);
-            recyclerView.setAdapter(adapter);
-
-            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-
-        });*/
+        profile_image = findViewById(R.id.profile_image);
+        username = findViewById(R.id.username);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
 
     }
--- app/src/main/java/com/example/chatapp/StartActivity.java
+++ app/src/main/java/com/example/chatapp/StartActivity.java
@@ -5,9 +5,13 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
 
     Button login, register;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,15 @@ public class StartActivity extends AppCompatActivity {
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
 
-        register.setOnClickListener(v -> {
-            ToolBox.openActivity(this, RegisterActivity.class);
-        });
-        login.setOnClickListener(v -> {
-            ToolBox.openActivity(this, LoginActivity.class);
-        });
+        register.setOnClickListener(v -> ToolBox.openActivity(this, RegisterActivity.class));
+        login.setOnClickListener(v -> ToolBox.openActivity(this, LoginActivity.class));
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+//        check if user is null
+        if (firebaseUser != null) {
+            ToolBox.openActivity(this, MainActivity.class);
+        }
 
 
     }
