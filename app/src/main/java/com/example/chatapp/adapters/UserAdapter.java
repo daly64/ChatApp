d001336372e1375fd617304ef8a7dc04074e809d
Daly <41022166+daly64@users.noreply.github.com>
2021-01-27 20:15:51 +0100

    adding display users

---
+++ app/src/main/java/com/example/chatapp/adapters/UserAdapter.java
@@ -0,0 +1,61 @@
+package com.example.chatapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.chatapp.R;
+import com.example.chatapp.models.User;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private Context context;
+    private List<User> users;
+
+    public UserAdapter(Context context, List<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.user_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")) {
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        } else {
+            Glide.with(context).load(user.getImageURL()).into(holder.profile_image);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView profile_image;
+        public TextView username;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            profile_image = itemView.findViewById(R.id.profile_image);
+            username = itemView.findViewById(R.id.username);
+        }
+    }
+}
