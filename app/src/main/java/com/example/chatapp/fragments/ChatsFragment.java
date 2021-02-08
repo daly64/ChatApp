62a8967f3ffac7308d13aeca8eb3bdf04a381458
Daly <41022166+daly64@users.noreply.github.com>
2021-02-08 12:46:39 +0100

    adding current chat managing system

--- app/src/main/java/com/example/chatapp/fragments/ChatsFragment.java
+++ app/src/main/java/com/example/chatapp/fragments/ChatsFragment.java
@@ -5,16 +5,93 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatapp.R;
+import com.example.chatapp.adapters.UserAdapter;
+import com.example.chatapp.models.Message;
+import com.example.chatapp.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatsFragment extends Fragment {
+    public RecyclerView recycler_view;
+    private UserAdapter userAdapter;
+    private List<User> users;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        View view = inflater.inflate(R.layout.fragment_chats, container, false);
+        recycler_view = view.findViewById(R.id.recycler_view);
+        recycler_view.setHasFixedSize(true);
+        recycler_view.setLayoutManager(new LinearLayoutManager(getContext()));
+        users = new ArrayList<>();
+        readUsers();
+        return view;
+
     }
+
+    private void readUsers() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference userReference = FirebaseDatabase.getInstance().getReference("Users");
+        DatabaseReference messagesReference = FirebaseDatabase.getInstance().getReference("chats");
+        messagesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    Message message = data.getValue(Message.class);
+                    if (message.getReceiver().equals(currentUser.getUid())) {
+                        //------------------------------------------------------------------
+                        userReference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                users.clear();
+                                for (DataSnapshot data : snapshot.getChildren()) {
+                                    User user = data.getValue(User.class);
+                                    if (user.getId().equals(message.getSender())) {
+                                        if (!users.contains(user)){
+                                            users.add(user);
+                                        }
+
+                                    }
+                                }
+                                userAdapter = new UserAdapter(getContext(), users);
+                                recycler_view.setAdapter(userAdapter);
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                        //------------------------------------------------------------------
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
 }
